5c996830e8bd5aff287781865b526bfc717e58e8
DenysDenysenko0 <148973568+DenysDenysenko0@users.noreply.github.com>
2026-03-02 22:24:37 +0200

    Implement toString and equals methods

--- src/main/java/sumdu/edu/ua/Clothes.java
+++ src/main/java/sumdu/edu/ua/Clothes.java
@@ -1,5 +1,55 @@
 package sumdu.edu.ua;
 
+import java.util.Objects;
+
 public class Clothes {
 
+    private String name;
+    private String size;
+    private double price;
+
+    public Clothes(String name, String size, double price) {
+        this.name = name;
+        this.size = size;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Clothes{" + "name='" + name + '\'' + ", size='" + size + '\'' + ", price=" + price + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Clothes)) return false;
+        Clothes clothes = (Clothes) o;
+        return Double.compare(clothes.price, price) == 0 &&
+                Objects.equals(name, clothes.name) &&
+                Objects.equals(size, clothes.size);
+    }
 }
