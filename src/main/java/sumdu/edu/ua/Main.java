f513e46dd11b2b809c3c0c11a14131fec9d254ff
DenysDenysenko0 <148973568+DenysDenysenko0@users.noreply.github.com>
2026-03-02 22:28:28 +0200

    Add main driver logic with array input

--- src/main/java/sumdu/edu/ua/Main.java
+++ src/main/java/sumdu/edu/ua/Main.java
@@ -1,17 +1,36 @@
 package sumdu.edu.ua;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть кількість елементів: ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        Clothes[] clothesArray = new Clothes[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Введення даних для одягу №" + (i + 1));
+            System.out.print("Назва: ");
+            String name = scanner.nextLine();
+            System.out.print("Розмір: ");
+            String size = scanner.nextLine();
+            System.out.print("Ціна: ");
+            double price = scanner.nextDouble();
+            scanner.nextLine();
+            clothesArray[i] = new Clothes(name, size, price);
         }
+
+        System.out.println("\nСписок одягу: ");
+        for (Clothes clothes : clothesArray) {
+            System.out.println(clothes);
+        }
+
+        scanner.close();
     }
 }
