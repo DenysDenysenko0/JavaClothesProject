98e869c593184f8807c0c02b05586e9c74214827
DenysDenysenko0 <148973568+DenysDenysenko0@users.noreply.github.com>
2026-03-10 00:01:47 +0200

    Added exception tests for constructor and setter validation

--- src/test/java/sumdu/edu/ua/ClothesTest.java
+++ src/test/java/sumdu/edu/ua/ClothesTest.java
@@ -1,5 +1,19 @@
 package sumdu.edu.ua;
 
-public class ClothesTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ClothesTest {
+
+    @Test
+    void shouldThrowExceptionWhenInvalidValueInSetter() {
+        Clothes clothes = new Clothes("T-Shirt", "M", 500.0, "Nike", 10);
+        assertThrows(IllegalArgumentException.class, () -> clothes.setPrice(-1));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenInvalidConstructorData() {
+        assertThrows(IllegalArgumentException.class, () -> new Clothes("", "M", -10.0, "", -5));
+    }
 }
