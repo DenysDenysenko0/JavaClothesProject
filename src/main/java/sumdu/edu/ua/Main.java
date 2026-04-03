355ba7f5dd1baf71736a7cdbd008b68f8dd2b725
DenysDenysenko0 <148973568+DenysDenysenko0@users.noreply.github.com>
2026-04-04 00:41:22 +0300

    Updated main class for loading objects from JSON on start and saving on exit

--- src/main/java/sumdu/edu/ua/Main.java
+++ src/main/java/sumdu/edu/ua/Main.java
@@ -13,8 +13,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Clothes> clothesList = new ArrayList<Clothes>();
+        List<Clothes> clothesList = ClothesJsonStorage.loadFromJson("input.json");
         boolean running = true;
+        System.out.println("Завантажено" + clothesList.size() + " об'єктів з JSON файлу");
 
         while (running) {
             printMenu();
@@ -24,6 +25,7 @@ public class Main {
                 case 1 -> createObjectMenu(scanner, clothesList);
                 case 2 -> printAll(clothesList);
                 case 3 -> {
+                    ClothesJsonStorage.saveToJson(clothesList, "input.json");
                     running = false;
                     System.out.println("Вихід");
                 }
